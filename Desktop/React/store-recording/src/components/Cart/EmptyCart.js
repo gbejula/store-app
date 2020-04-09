ed8016267b2c0c98c46a6ff7cd1b2bdd008ab702
gbejula <gbejula@gmail.com>
2020-04-09 17:30:14 +0100

    store app

---
+++ Desktop/React/store-recording/src/components/Cart/EmptyCart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function EmptyCart() {
+  return (
+    <div className="container mt-5">
+      <div className="col-10 mx-auto text-center">
+        <h1 className="text-uppercase">
+          your cart is currently empty
+        </h1>
+      </div>
+    </div>
+  )
+}
